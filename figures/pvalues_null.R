f2ba95260f6e05c2a80c3da23015ae886a6616c3
abigailkeller <abigail.g.keller1@gmail.com>
2025-04-07 20:27:38 +0000

    plots of p values

---
+++ figures/pvalues_null.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+
+# read in p values
+pvalues <- rbind(
+  as.data.frame(readRDS("pvalues/null/basic.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "basic", latent = "no"),
+  as.data.frame(readRDS("pvalues/null/cov_occ.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "cov_occ", latent = "no"),
+  as.data.frame(readRDS("pvalues/null/cov_occ_det.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "cov_occ_det", latent = "no"),
+  as.data.frame(readRDS("pvalues/null/sp_ranef.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "sp_ranef", latent = "no"),
+  as.data.frame(readRDS("pvalues/null/basic_latent.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "basic", latent = "yes"),
+  as.data.frame(readRDS("pvalues/null/cov_occ_latent.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "cov_occ", latent = "yes"),
+  as.data.frame(readRDS("pvalues/null/cov_occ_det_latent.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "cov_occ_det", latent = "yes"),
+  as.data.frame(readRDS("pvalues/null/sp_ranef_latent.rds")) %>% 
+    pivot_longer(cols = everything(), names_to = "discrepancy",
+                 values_to = "p_value") %>% 
+    mutate(model = "sp_ranef", latent = "yes")
+)
+
+model_labels <- c(
+  "basic" = "basic",
+  "cov_occ" = "occupancy\ncovariates",
+  "cov_occ_det" = "occupancy +\ndetection\ncovariates",
+  "sp_ranef" = "site random effect"
+)
+
+discrepancy_labels <- c(
+  "chi_sq" = "chi-squared",
+  "deviance" = "deviance",
+  "ftukey" = "Freeman-Tukey",
+  "lik_ratio" = "likelihood ratio"
+)
+
+plot_wlatent <- ggplot(pvalues, aes(x = p_value, fill = latent)) +
+  geom_histogram() +
+  scale_y_continuous(breaks = c(0, 50, 100)) +
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = c("0", "0.25", "0.5", "0.75", "1")) +
+  facet_grid(model ~ discrepancy, labeller = labeller(
+    model = model_labels,
+    discrepancy = discrepancy_labels
+  )) +
+  labs(x = "p value", y = "count") +
+  theme_minimal()
+ggsave("figures/null_pvalues_wlatent.png", dpi = 400, plot_wlatent,
+       width = 7, height = 5)
+
+plot <- ggplot(pvalues[pvalues$latent == "no",], 
+               aes(x = p_value)) +
+  geom_histogram() +
+  scale_y_continuous(breaks = c(0, 50, 100)) +
+  scale_x_continuous(breaks = c(0, 0.25, 0.5, 0.75, 1),
+                     labels = c("0", "0.25", "0.5", "0.75", "1")) +
+  facet_grid(model ~ discrepancy, labeller = labeller(
+    model = model_labels,
+    discrepancy = discrepancy_labels
+  )) +
+  labs(x = "p value", y = "count") +
+  theme_minimal()
+ggsave("figures/null_pvalues.png", dpi = 400, plot,
+       width = 7, height = 5)
---
+++ occupancy_model_latent.R
@@ -0,0 +1,425 @@
+
+####################
+# model variations #
+####################
+
+
+# 1. basic version
+# 2-3. covariates on detection and occupancy
+# 4. spatial random effect
+
+
+# 1. basic occupancy model
+model_basic_latent <- nimbleCode({
+  
+  psi ~ dbeta(1, 1)
+  p ~ dbeta(1, 1)
+  
+  for (i in 1:nSites) {
+    
+    z[i] ~ dbern(psi) 
+    
+    # posterior predictive replicated data
+    z_rep[i] ~ dbern(psi)
+    
+    for (j in 1:nVisits) {
+      
+      # observed data
+      y[i, j] ~ dbern(z[i] * p)
+      
+      # log deviance - observed data
+      D_obs[i, j] <- -2 * log(dbinom(y[i, j], 1, z[i] * p) + 1e-6)
+      
+      # posterior predictive replicated data
+      y_rep[i, j] ~ dbern(z_rep[i] * p)
+      
+      # deviance - pp data
+      D_rep[i, j] <- -2 * log(dbinom(y_rep[i, j], 1, z[i] * p) + 1e-6)
+      
+    }
+    
+    # expected values
+    y_exp[i, 1:nVisits] <- z[i] * p
+    
+  }
+  
+  ##############################
+  # total discrepancy measures #
+  ##############################
+  
+  # deviance
+  D_obs_total <- sum(D_obs[1:nSites, 1:nVisits])
+  D_rep_total <- sum(D_rep[1:nSites, 1:nVisits])
+
+  # chi-squared
+  chi_obs_total <- calc_chi(y[1:nSites, 1:nVisits],
+                            y_exp[1:nSites, 1:nVisits])
+  chi_rep_total <- calc_chi(y_rep[1:nSites, 1:nVisits],
+                            y_exp[1:nSites, 1:nVisits])
+
+  # likelihood ratio
+  ratio_obs_total <- calc_ratio(y[1:nSites, 1:nVisits],
+                                y_exp[1:nSites, 1:nVisits])
+  ratio_rep_total <- calc_ratio(y_rep[1:nSites, 1:nVisits],
+                                y_exp[1:nSites, 1:nVisits])
+
+  # freeman tukey
+  tukey_obs_total <- calc_tukey(y[1:nSites, 1:nVisits],
+                                y_exp[1:nSites, 1:nVisits])
+  tukey_rep_total <- calc_tukey(y_rep[1:nSites, 1:nVisits],
+                                y_exp[1:nSites, 1:nVisits])
+
+  
+})
+
+# 2. covariates on occupancy
+model_cov_occ_latent <- nimbleCode({
+  
+  p ~ dunif(0, 1)
+  for (c in 1:ncov) {
+    beta[c] ~ dunif(-10, 10)
+  }
+  
+  # logit link - site-level covariates
+  psi[1:nSites] <- ilogit(x_site[1:nSites, 1:ncov] %*% beta[1:ncov])
+  
+  
+  for (i in 1:nSites) {
+    
+    z[i] ~ dbern(psi[i])
+    
+    # posterior predictive replicated data
+    z_rep[i] ~ dbern(psi[i])
+    
+    for (j in 1:nVisits) {
+      
+      # observed data
+      y[i, j] ~ dbern(z[i] * p)
+      
+      # log deviance for observed data
+      D_obs[i, j] <- -2 * log(dbinom(y[i, j], 1, z[i] * p) + 1e-6) 
+      
+      # posterior predictive replicated data
+      y_rep[i, j] ~ dbern(z_rep[i] * p)
+      
+      # deviance - pp data
+      D_rep[i, j] <- -2 * log(dbinom(y_rep[i, j], 1, z[i] * p) + 1e-6)
+      
+    }
+    
+    # expected values
+    y_exp[i, 1:nVisits] <- z[i] * p
+    
+  }
+  
+  ##############################
+  # total discrepancy measures #
+  ##############################
+  
+  # deviance
+  D_obs_total <- sum(D_obs[1:nSites, 1:nVisits])
+  D_rep_total <- sum(D_rep[1:nSites, 1:nVisits])
+  
+  # chi-squared
+  chi_obs_total <- calc_chi(y[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  chi_rep_total <- calc_chi(y_rep[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  
+  # likelihood ratio
+  ratio_obs_total <- calc_ratio(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  ratio_rep_total <- calc_ratio(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+  # freeman tukey
+  tukey_obs_total <- calc_tukey(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  tukey_rep_total <- calc_tukey(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+})
+
+# 3. covariates on occupancy and detection
+model_cov_occ_det_latent <- nimbleCode({
+  
+  for (c in 1:ncov_o) {
+    beta_o[c] ~ dunif(-10, 10)
+  }
+  for (c in 1:ncov_p) {
+    beta_p[c] ~ dunif(-10, 10)
+  }
+  
+  # logit link - site-level covariates
+  psi[1:nSites] <- ilogit(x_site[1:nSites, 1:ncov_o] %*% beta_o[1:ncov_o])
+  
+  for (i in 1:nSites) {
+    
+    z[i] ~ dbern(psi[i])
+    
+    # posterior predictive replicated data
+    z_rep[i] ~ dbern(psi[i])
+    
+    # logit link - detection-level covariates
+    p[i, 1:nVisits] <- ilogit(x_visit[i, 1:nVisits, 1:ncov_p] %*%
+                                beta_o[1:ncov_p])
+    
+    for (j in 1:nVisits) {
+      
+      # observed data
+      y[i, j] ~ dbern(z[i] * p[i, j])
+      
+      # log deviance for observed data
+      D_obs[i, j] <- -2 * log(dbinom(y[i, j], 1, z[i] * p[i, j]) + 1e-6) 
+      
+      # posterior predictive replicated data
+      y_rep[i, j] ~ dbern(z_rep[i] * p[i, j])
+      
+      # deviance - pp data
+      D_rep[i, j] <- -2 * log(dbinom(y_rep[i, j], 1, z[i] * p[i, j]) + 1e-6)
+      
+      # expected values
+      y_exp[i, j] <- z[i] * p[i, j]
+      
+    }
+  }
+  
+  ##############################
+  # total discrepancy measures #
+  ##############################
+  
+  # deviance
+  D_obs_total <- sum(D_obs[1:nSites, 1:nVisits])
+  D_rep_total <- sum(D_rep[1:nSites, 1:nVisits])
+  
+  # chi-squared
+  chi_obs_total <- calc_chi(y[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  chi_rep_total <- calc_chi(y_rep[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  
+  # likelihood ratio
+  ratio_obs_total <- calc_ratio(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  ratio_rep_total <- calc_ratio(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+  # freeman tukey
+  tukey_obs_total <- calc_tukey(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  tukey_rep_total <- calc_tukey(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+})
+
+# 4. spatial random effect
+model_spatial_ranef_latent <- nimbleCode({
+  
+  psi_mean ~ dunif(-10, 10)
+  psi_sd ~ dunif(0, 100)
+  p ~ dbeta(1, 1)
+  
+  psi[1:nRegions] <- ilogit(psi_logit[1:nRegions])
+  
+  for(r in 1:nRegions){
+    
+    psi_logit[r] ~ dnorm(psi_mean, psi_sd)
+    
+    # posterior predictive replicated data
+    psi_logit_rep[r] ~ dnorm(psi_mean, psi_sd)
+    
+  }
+  
+  for(i in 1:nSites) {
+    
+    z[i] ~ dbern(psi[region[i]])
+    
+    # posterior predictive replicated data
+    z_rep[i] ~ dbern(ilogit(psi_logit_rep[region[i]]))
+    
+    for(j in 1:nVisits) {
+      
+      # observed data
+      y[i, j] ~ dbern(z[i] * p)
+      
+      # log deviance for observed data
+      D_obs[i, j] <- -2 * log(dbinom(y[i, j], 1, z[i] * p) + 1e-6)  
+      
+      # posterior predictive replicated data
+      y_rep[i, j] ~ dbern(z_rep[i] * p)
+      
+      # deviance - pp data
+      D_rep[i, j] <- -2 * log(dbinom(y_rep[i, j], 1, z[i] * p) + 1e-6)
+      
+    }
+    
+    # expected values
+    y_exp[i, 1:nVisits] <- z[i] * p
+    
+  }
+  
+  ##############################
+  # total discrepancy measures #
+  ##############################
+  
+  # deviance
+  D_obs_total <- sum(D_obs[1:nSites, 1:nVisits])
+  D_rep_total <- sum(D_rep[1:nSites, 1:nVisits])
+  
+  # chi-squared
+  chi_obs_total <- calc_chi(y[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  chi_rep_total <- calc_chi(y_rep[1:nSites, 1:nVisits], 
+                            y_exp[1:nSites, 1:nVisits])
+  
+  # likelihood ratio
+  ratio_obs_total <- calc_ratio(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  ratio_rep_total <- calc_ratio(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+  # freeman tukey
+  tukey_obs_total <- calc_tukey(y[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  tukey_rep_total <- calc_tukey(y_rep[1:nSites, 1:nVisits], 
+                                y_exp[1:nSites, 1:nVisits])
+  
+})
+
+
+######################################
+# functions for discrepancy measures #
+######################################
+
+calc_chi <- nimbleFunction(
+  
+  run = function(y = double(2), 
+                 y_exp = double(2))
+  {
+    returnType(double(0))
+    
+    # calculate chi squared discrepancy measure
+    chi_out <- (y - y_exp) ^ 2 / (y + 1e-6)
+    
+    return(sum(chi_out))
+  }
+)
+assign("calc_chi", calc_chi, envir = .GlobalEnv)
+
+calc_ratio <- nimbleFunction(
+  
+  run = function(y = double(2), 
+                 y_exp = double(2))
+  {
+    returnType(double(0))
+    
+    # calculate likelihood ratio discrepancy measure
+    ratio_out <- 2 * y * log((y + 1e-6) / (y_exp + 1e-6))
+    
+    return(sum(ratio_out))
+  }
+)
+assign("calc_ratio", calc_ratio, envir = .GlobalEnv)
+
+calc_tukey <- nimbleFunction(
+  
+  run = function(y = double(2), 
+                 y_exp = double(2))
+  {
+    returnType(double(0))
+    
+    # calculate freeman tukey discrepancy measure
+    tukey_out <- (sqrt(y) - sqrt(y_exp)) ^ 2
+    
+    return(sum(tukey_out))
+  }
+)
+assign("calc_tukey", calc_tukey, envir = .GlobalEnv)
+
+##############################
+# functions for running MCMC #
+##############################
+
+fit_basic_latent <- function(compiled_model, compiled_mcmc,
+                             niter, nburnin, thin) {
+  
+  # function for generating initial values
+  inits <- function(y) list(psi = runif(1, 0, 1), p = runif(1, 0, 1), 
+                            z = pmin(rowSums(y), 1),
+                            z_rep = rep(NA, nrow(y)),
+                            y_rep = matrix(NA, nrow(y), ncol(y))) 
+  
+  # add initial values
+  compiled_model$setInits(inits(compiled_model$y))
+  
+  # generate samples
+  samples <- runMCMC(compiled_mcmc, niter = niter, 
+                     nburnin = nburnin, thin = thin)
+  
+  return(samples)
+}
+
+
+fit_cov_occ_latent <- function(compiled_model, compiled_mcmc, ncov,
+                               niter, nburnin, thin) {
+  
+  # function for generating initial values
+  inits <- function(y, ncov) list(beta = rnorm(ncov, 0, 1), 
+                                  p = runif(1, 0, 1), 
+                                  z = pmin(rowSums(y), 1),
+                                  z_rep = rep(NA, nrow(y)),
+                                  y_rep = matrix(NA, nrow(y), ncol(y)))  
+  
+  # add initial values
+  compiled_model$setInits(inits(compiled_model$y, ncov))
+  
+  # generate samples
+  samples <- runMCMC(compiled_mcmc, niter = niter, 
+                     nburnin = nburnin, thin = thin)
+  
+  return(samples)
+}
+
+
+fit_cov_occ_det_latent <- function(compiled_model, compiled_mcmc, ncov_o, 
+                                   ncov_p, niter, nburnin, thin) {
+  
+  # function for generating initial values
+  inits <- function(y, ncov_o, ncov_p) list(beta_o = rnorm(ncov_o, 0, 1), 
+                                            beta_p = rnorm(ncov_p, 0, 1), 
+                                            z_rep = rep(NA, nrow(y)),
+                                            z = pmin(rowSums(y), 1),
+                                            y_rep = matrix(NA, nrow(y), 
+                                                           ncol(y)))  
+  
+  # add initial values
+  compiled_model$setInits(inits(compiled_model$y, ncov_o, ncov_p))
+  
+  # generate samples
+  samples <- runMCMC(compiled_mcmc, niter = niter, 
+                     nburnin = nburnin, thin = thin)
+  
+  return(samples)
+}
+
+fit_spatial_ranef_latent <- function(compiled_model, compiled_mcmc, region, 
+                                     nRegions, niter, nburnin, thin) {
+  
+  # function for generating initial values
+  inits <- function(y, nRegions) list(
+    psi_mean = 0, psi_sd = 1, p = runif(1, 0, 1),
+    psi_logit = logit(runif(nRegions, 0, 1)), 
+    z = pmin(rowSums(y), 1), z_rep = rep(NA, nrow(y)),
+    y_rep = matrix(NA, nrow(y), ncol(y))
+  )  
+  
+  # add initial values
+  compiled_model$setInits(inits(compiled_model$y, nRegions))
+  
+  # generate samples
+  samples <- runMCMC(compiled_mcmc, niter = niter, 
+                     nburnin = nburnin, thin = thin)
+  
+  return(samples)
+}
+
